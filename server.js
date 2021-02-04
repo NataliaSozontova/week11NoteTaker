70e52c3908d92937c1ad7842b981455b078e3502
Natalia <nasozontova@gmail.com>
2021-02-04 18:19:51 -0500

    updating folder structure

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('./Develop/public'))
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./Develop/routes/apiRoutes')(app);
+require('./Develop/routes/htmlRoutes')(app);
 
 
 
