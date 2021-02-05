592ee7cf74284a2d4d4c26ee8ea35d89fc8a6fb9
Natalia <nasozontova@gmail.com>
2021-02-04 21:42:20 -0500

    updating

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./Develop/public'))
+app.use(express.static('./public'))
 
-require('./Develop/routes/apiRoutes')(app);
-require('./Develop/routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 
 
